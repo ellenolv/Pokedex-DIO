cca7cf24de790afb3ccd4ea42965ca1d6052b214
ellenolv <ellendeoliveirapereira>
2023-10-07 12:12:46 -0300

    alteração no estilo da pagina de detalhes do pokemon

--- assets/js/details.js
+++ assets/js/details.js
@@ -5,60 +5,46 @@ if (storedPokemonDetails) {
     const pokemon = JSON.parse(storedPokemonDetails);
     const detailsContainer = document.getElementById('detailsContainer');
     detailsContainer.innerHTML = `
-    <h2>${pokemon.name}</h2>
+    <h2 class="name">${pokemon.name}</h2>
+    <p class="number"> #${pokemon.number}</p>
     <img src="${pokemon.photo}" alt="${pokemon.name}">
-    <p>Number: #${pokemon.number}</p>
-    <p>Types: ${pokemon.types.join(', ')}</p>
-    <p>Height: ${pokemon.height}</p>
-    <p>Weight: ${pokemon.weight}</p>
-    <h3>Stats:</h3>
-    <table>
-        <tr>
-            <th>Name</th>
-            <th>Base Stat</th>
-            <th>Progress</th>
-        </tr>
-        ${Object.keys(pokemon.stats).map((statName) => `
+    
+    <div class="detail">
+        <ol class="types "> 
+            ${pokemon.types.map((type) => `<li  class="type ${type}">${type}</li>`).join(' ')}
+        </ol>
+        <p class="height">Height: ${convertHeightToMeters(pokemon.height)}</p>
+    <p class="weight">Weight:${convertWeightToKg(pokemon.weight)}</p>
+        <table class="progress-table">
             <tr>
-                <td>${statName}</td>
-                <td>${pokemon.stats[statName].base_stat}</td>
-                <td>
-                    <progress value="${pokemon.stats[statName].base_stat}" max="150"></progress>
-                </td>
+                <th>Name</th>
+                <th>Base Stat</th>
+                <th>Progress</th>
             </tr>
-        `).join('')}
-    </table>
-`;
+                ${Object.keys(pokemon.stats).map((statName) => `
+                <tr>
+                    <td>${statName}</td>
+                    <td>${pokemon.stats[statName].base_stat}</td>
+                    <td class="progress">
+                        <progress class=" progress-bar${pokemon.type}" value="${pokemon.stats[statName].base_stat}" max="150"></progress>
+                    </td>
+                </tr>
+            `).join('')}
+        </table>
+    </div>`
+
 console.log(pokemon);
 } else {
     // Lidar com o caso em que os detalhes do Pokémon não estão disponíveis
     console.error('Detalhes do Pokémon não encontrados.');
 }
 
-
-function proximo(){
-    if(pokemon.number < 1008){
-        pokemon.number ++ ;
-    }
-    else{
-        pokemon.number = 1;
-    }
+// Função para converter a altura para metros (m)
+function convertHeightToMeters(heightInDecimetres) {
+    return (heightInDecimetres / 10).toFixed(2) + ' m';
 }
-
-function anterior(){
-    if(pokemon.number > 1){
-        pokemon.number --;
-    }
-    else{
-        pokemon.number = 1008;
-    }
+// Função para converter o peso para quilogramas (kg)
+function convertWeightToKg(weightInDecagrams) {
+    return (weightInDecagrams / 10).toFixed(2) + ' kg';
 }
-
-function convertHeightToMeters(){
-    return this.pokemon.height / 10 + 'm';
-  }
-
-function convertWeightToKg(){
-    return this.pokemon.weight / 10 + 'kg';
-  }
  
